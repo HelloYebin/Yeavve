b54f11788cea09bf6a085627f45fd4db90743a9b
HelloYebin <104414179+HelloYebin@users.noreply.github.com>
2022-08-04 18:31:42 +0900

    first commit

---
+++ src/component/header.jsx
@@ -0,0 +1,32 @@
+import styles from "../style/header.css";
+import logo from "../image/logo.png";
+import search from "../image/icons8-search-50.png";
+function Header() {
+  return (
+    <div className="position">
+      <div className="header">
+        <ul className="header-links">
+          <li>로그인</li>|<li>이용권</li>|<li>쿠폰·코인 ∨</li>|<li>웨이브온</li>
+          |<li>이벤트</li>
+        </ul>
+        <hr className="hr" />
+        <div className="secondHeader">
+          <img src={logo} className="logo" />
+          <nav>
+            <ul id="menu">
+              <li>홈</li>
+              <li>카테고리</li>
+              <li>LIVE</li>
+              <li>MY</li>
+            </ul>
+          </nav>
+          <div className="searchBar">
+            <img src={search} width="25px" />
+            <span className="search">검색하기</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Header;
