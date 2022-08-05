bc63c4bbc994780d06a22737a6247d179b432a6b
HelloYebin <104414179+HelloYebin@users.noreply.github.com>
2022-08-05 17:51:57 +0900

    add onMouse function

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
-import img from "../image/logo.png";
+import arrow from "../image/arrow.png";
 import styles from "../style/main.module.css";
 
 function Main() {
   const [movies, setMovies] = useState([]);
+  const [showArrow, setShowArrow] = useState(false);
+  function setArrow() {
+    setShowArrow(true);
+  }
+  function noneArrow() {
+    setShowArrow(false);
+  }
 
   useEffect(() => {
     fetch(
@@ -11,16 +18,29 @@ function Main() {
     )
       .then((response) => response.json())
       .then((json) => setMovies(json.data.movies));
-    console.log(movies);
   }, []);
 
   return (
-    <div>
+    <div
+      id={styles.movieContainer}
+      onMouseEnter={() => setArrow()}
+      onMouseLeave={() => noneArrow()}
+    >
       <div className={styles.moviesTop}>
         <a className={styles.topic}>실시간 인기 프로그램</a>
         <a className={styles.more}>더보기 ＞</a>
       </div>
       <section className={styles.movies}>
+        <img
+          src={arrow}
+          className={styles.leftArrow}
+          style={{ left: showArrow ? "0px" : null }}
+        />
+        <img
+          src={arrow}
+          className={styles.rightArrow}
+          style={{ right: showArrow ? "0px" : null }}
+        />
         {movies.map((movie) => (
           <div key={movie.id} className={styles.movie}>
             <div className={styles.imgBox}>
