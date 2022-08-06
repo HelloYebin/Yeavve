a9766ff7ae09aae5213895e1a33211890b79dae9
HelloYebin <104414179+HelloYebin@users.noreply.github.com>
2022-08-06 20:25:44 +0900

    add slideFunction, View value each API

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ function App() {
     <div>
       <Header />
       <Banner />
-      <Main />
-      <Main />
-      <Main />
+      <Main topic={"인기영화"} detail={"minimum_rating=8"} />
+      <Main topic={"평점 9점이상의 명작영화"} detail={"minimum_rating=9"} />
+      <Main
+        topic={"다음 이야기가 궁금해지는 미스터리물"}
+        detail={"minimum_rating=8&genre=Thriller"}
+      />
+      <Main
+        topic={"가족과 함께 보는 영화"}
+        detail={"minimum_rating=8&genre=Family"}
+      />
     </div>
   );
 }
--- src/component/main.js
+++ src/component/main.js
@@ -2,20 +2,28 @@ import { useEffect, useState } from "react";
 import arrow from "../image/arrow.png";
 import styles from "../style/main.module.css";
 
-function Main() {
+function Main({ topic, rating, genre, detail }) {
   const [movies, setMovies] = useState([]);
   const [showArrow, setShowArrow] = useState(false);
+  let [movieX, setMovieX] = useState(0);
+
   function setArrow() {
     setShowArrow(true);
   }
   function noneArrow() {
     setShowArrow(false);
   }
+  function setRight(e) {
+    if (movieX <= 7000) {
+      setMovieX((movieX += 480));
+    }
+  }
+  function setLeft(e) {
+    setMovieX((movieX -= 480));
+  }
 
   useEffect(() => {
-    fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    )
+    fetch(`https://yts.mx/api/v2/list_movies.json?${detail}&sort_by=year`)
       .then((response) => response.json())
       .then((json) => setMovies(json.data.movies));
   }, []);
@@ -27,28 +35,33 @@ function Main() {
       onMouseLeave={() => noneArrow()}
     >
       <div className={styles.moviesTop}>
-        <a className={styles.topic}>실시간 인기 프로그램</a>
+        <a className={styles.topic}>{topic}</a>
         <a className={styles.more}>더보기 ＞</a>
       </div>
-      <section className={styles.movies}>
-        <img
-          src={arrow}
-          className={styles.leftArrow}
-          style={{ left: showArrow ? "0px" : null }}
-        />
-        <img
-          src={arrow}
-          className={styles.rightArrow}
-          style={{ right: showArrow ? "0px" : null }}
-        />
+      <img
+        src={arrow}
+        className={styles.leftArrow}
+        style={{ left: showArrow ? "0px" : null }}
+        onClick={() => setLeft()}
+      />
+      <img
+        src={arrow}
+        className={styles.rightArrow}
+        style={{ right: showArrow ? "0px" : null }}
+        onClick={() => setRight()}
+      />
+      <section
+        className={styles.movies}
+        style={{ paddingRight: `${movieX}px` }}
+      >
         {movies.map((movie) => (
           <div key={movie.id} className={styles.movie}>
             <div className={styles.imgBox}>
               <img src={movie.medium_cover_image} className={styles.movieImg} />
             </div>
             <a>
-              {movie.title.length > 27
-                ? `${movie.title.slice(0, 27)}...`
+              {movie.title.length > 26
+                ? `${movie.title.slice(0, 26)}...`
                 : movie.title}
             </a>
           </div>
